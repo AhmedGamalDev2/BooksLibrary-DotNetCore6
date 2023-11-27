3825900aa5b907b9703976d205683782ea68aa3f
AhmedGamalDev2 <ahmedgamal99123@gmail.com>
2023-11-27 11:22:43 +0200

    Add Datatable

---
+++ Bookify.Web/Helpers/ActiveTag.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Bookify.Web.Helpers
+{
+    [HtmlTargetElement("a", Attributes = "active-when")]
+    public class ActiveTag : TagHelper
+    {
+        //receive tag value like => active-when ="Categories" => ActiveWhen = Categories
+        public string? ActiveWhen { get; set; }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext? ViewContextData { get; set; } //receive all viewcontext
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            //output receive the element that we put new helper tag(active-when) in it like anchor tag (a)
+            if(string.IsNullOrEmpty(ActiveWhen))
+                return;
+
+            var currentController = ViewContextData?.RouteData.Values["controller"]?.ToString();
+
+            if (currentController!.Equals(ActiveWhen))
+            {   //in case the element(like anchor tag) contains class attribute
+                if (output.Attributes.ContainsName("class"))
+                    output.Attributes.SetAttribute("class", $"{output.Attributes["class"].Value} active");
+                else
+					//in case the element(like anchor tag) does not contain class attribute
+					output.Attributes.SetAttribute("class", "active");
+            }
+        }
+    }
+}
--- Bookify.Web/wwwroot/js/site.js
+++ Bookify.Web/wwwroot/js/site.js
@@ -1,9 +1,17 @@
-﻿function ShowSweetAlertSuccessMessage(successMessage) {
+﻿// Shared variables
+var table;
+var datatable;
+var exportedCols = [];
+
+
+//sweetalert
+function ShowSweetAlertSuccessMessage(successMessage) {
     Swal.fire({
         icon: "success",
         title: successMessage,
     });
 }
+//toaster alert
 function ShowToastrSuccessMessage(successMessage) {
     toastr["success"](successMessage)
 
@@ -25,8 +33,115 @@ function ShowToastrSuccessMessage(successMessage) {
         "hideMethod": "fadeOut"
     }
 }
+//datatables
+var headers = $('th');
+$.each(headers, function (i) { // select columns that i need to not export
+    if (!$(this).hasClass('js-no-export'))
+        exportedCols.push(i);
+});
+// Class definition
+var KTDatatables = function () {
+
+    // Private functions
+    var initDatatable = function () {
+
+        // Init datatable --- more info on datatables: https://datatables.net/manual/
+        datatable = $(table).DataTable({
+            "info": false,
+            'order': [],
+            'pageLength': 10,
+            'columnDefs': [
+                { 'orderable': false, 'targets': -1 },  // آخر عمود (أزرار العمليات)
+               // { 'orderable': false, 'targets': -2 }   // قبل آخر عمود (أزرار الحالة)
+            ]
+        });
+    }
+
+    // Hook export buttons
+    var exportButtons = () => {
+        const documentTitle = $('.js-datatables').data('datatable-title');
+        var buttons = new $.fn.dataTable.Buttons(table, {
+            buttons: [
+                {
+                    extend: 'copyHtml5',
+                    title: documentTitle,
+                    exportOptions: {
+                        columns: [0,1,2,3]  // تحديد الأعمدة التي يمكن تضمينها في التصدير
+                    }
+                },
+                {
+                    extend: 'excelHtml5',
+                    title: documentTitle,
+                    exportOptions: { // دي دريقة 
+                        columns: [0, 1, 2, 3] //  //  تحديد الأعمدة التي يمكن تضمينها في التصدير
+                    }
+                },
+                {
+                    extend: 'csvHtml5',
+                    title: documentTitle,
+                    exportOptions: {
+                        columns: exportedCols
+                    }
+                },
+                {
+                    extend: 'pdfHtml5',
+                    title: documentTitle,
+                    exportOptions: { // ودي طريقة تانية 
+                        columns: exportedCols
+                    },
+                 
+                }
+            ]
+        }).container().appendTo($('#kt_datatable_example_buttons'));
+
+        // Hook dropdown menu click event to datatable export buttons
+        const exportButtons = document.querySelectorAll('#kt_datatable_example_export_menu [data-kt-export]');
+        exportButtons.forEach(exportButton => {
+            exportButton.addEventListener('click', e => {
+                e.preventDefault();
+
+                // Get clicked export value
+                const exportValue = e.target.getAttribute('data-kt-export');
+                const target = document.querySelector('.dt-buttons .buttons-' + exportValue);
+
+                // Trigger click event on hidden datatable export buttons
+                target.click();
+            });
+        });
+    }
+
+    // Search Datatable --- official docs reference: https://datatables.net/reference/api/search()
+    var handleSearchDatatable = () => {
+        const filterSearch = document.querySelector('[data-kt-filter="search"]');
+        filterSearch.addEventListener('keyup', function (e) {
+            datatable.search(e.target.value).draw();
+        });
+    }
+
+    // Public methods
+    return {
+        init: function () {
+            table = document.querySelector('.js-datatables');
+
+            if (!table) {
+                return;
+            }
+
+            initDatatable();
+            exportButtons();
+            handleSearchDatatable();
+        }
+    };
+}();
+
 
 $(document).ready(function () {
+    /**start datatable*/
+    //$('table').DataTable();
+    KTUtil.onDOMContentLoaded(function () {
+        KTDatatables.init();
+    });
+    /**end datatable*/
     var successMessage = $('#SuccessMessage').text();
     //attention to space in successMessage because is not == ""
     if (successMessage !== "") {
